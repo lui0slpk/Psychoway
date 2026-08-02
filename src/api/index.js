8117893bb9a0bbbfdc557653dce5adb3b9f37b94
lui0slpk <itslucky535@gmail.com>
2026-08-02 00:11:37 -0500

    feat(api): export full module surface from barrel

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,15 @@
  * Uso:
  *   import { diaryApi, objectivesApi } from "../api";
  */
+export { default as authApi } from "./auth.api";
+export { default as usersApi } from "./users.api";
 export { default as diaryApi } from "./diary.api";
 export { default as objectivesApi } from "./objectives.api";
+export { default as emotionsApi } from "./emotions.api";
 export { default as meetingsApi } from "./meetings.api";
 export { default as psychobotApi } from "./psychobot.api";
-export { default as usersApi } from "./users.api";
 export { default as trackingApi } from "./tracking.api";
 export { default as notificationsApi } from "./notifications.api";
+export { default as psychologistsApi } from "./psychologists.api";
+export { request, ApiError } from "./client";
 export { API_URL, PUBLIC_URL } from "./config";
